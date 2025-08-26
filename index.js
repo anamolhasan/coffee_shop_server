d1f0b3753ece1f2e26efe92a47fadc22ef74f882
anamolhasan <mdanamolhasan1998@gmail.com>
2025-08-27 01:16:31 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port http://localhost:${port}`)
+})
