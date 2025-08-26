28645aeb17da2dcbf88f55ed95da8bb6d364a844
anamolhasan <mdanamolhasan1998@gmail.com>
2025-08-27 05:00:12 +0600

    coffee data post route added

--- index.js
+++ index.js
@@ -22,10 +22,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    
+    const coffeeCollection = client.db('coffeeDB').collection('recapCoffees')
+    
 
 
+   app.post('/coffees', async (req, res) => {
+      const newCoffee = req.body
+      // console.log(coffeeData)
 
-
+      const result = await coffeeCollection.insertOne(newCoffee)
+      res.send(result)
+   })
 
 
 
